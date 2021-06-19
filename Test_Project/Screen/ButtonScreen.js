de4dd7b35b4429f59e6cbb5f2da5dd8994c3dda1
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-20 03:39:32 +0600

    Button

--- Test_Project/Screen/ButtonScreen.js
+++ Test_Project/Screen/ButtonScreen.js
@@ -1,2 +1,12 @@
-import Raact from 'react';
-import {  } from 'react-native';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+const ButtonScreen = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+      <Button title="Submit" onPress={()=> console.log('Click')}/>
+    </View>
+  );
+};
+export default ButtonScreen;
