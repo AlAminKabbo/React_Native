464dbefb8786ef930406916adc3d281136aad160
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-20 02:33:06 +0600

    react native basic

---
+++ Test_Project/Screen/NativeFeedback.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TouchableNativeFeedback, StatusBar } from "react-native";
+
+const NativeFeedback = () => {
+  const [rippleColor, setRippleColor] = useState(randomHexColor());
+  const [rippleOverflow, setRippleOverflow] = useState(false);
+  return (
+    <View style={styles.container}>
+      <TouchableNativeFeedback
+        onPress={() => {
+          setRippleColor(randomHexColor());
+          setRippleOverflow(!rippleOverflow);
+        }}
+        background={TouchableNativeFeedback.Ripple(rippleColor, rippleOverflow)}
+      >
+        <View style={styles.touchable}>
+          <Text style={styles.text}>TouchableNativeFeedback</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+};
+
+const randomHexColor = () => {
+  return "#000000".replace(/0/g, function() {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  touchable: { flex: 0.5, borderColor: "black", borderWidth: 1 },
+  text: { alignSelf: "center" }
+});
+
+export default NativeFeedback;
---
+++ Test_Project/Screen/ViewScreen.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {
+   Text,
+   View,
+ } from 'react-native';
+ 
+ const ViewScreen = () => {
+  return(
+      <View>
+      <Text>Hello world</Text>
+      </View>
+  )
+ };
+ 
+ export default ViewScreen;
+ 
