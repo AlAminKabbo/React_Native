c2804991347460d26815252294ed6b1b31f60c3f
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-22 04:46:26 +0600

    Update App.js

--- Test_Project/App.js
+++ Test_Project/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import CustomAlert from './Screen/CustomAlert';
 
-const App = () => {
+import {View, Text, StyleSheet} from 'react-native';
+
+const Profile = ({name, phoneNumber}) => {
   return (
-    // <ImageScreen />
-    <CustomAlert />
+    <View style={styles.myView}>
+      <Text>{name}</Text>
+      <Text>{phoneNumber}</Text>
+    </View>
   );
 };
 
+function App() {
+  return (
+    <View>
+      <Profile name="Al Amin" phoneNumber="01728861499" />
+      <Profile name="Nitish" phoneNumber="01700000000" />
+      <Profile name="Maruf" phoneNumber="01911111111" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  myView: {
+    backgroundColor: 'cyan',
+    padding: 20,
+    margin: 5,
+  },
+});
 export default App;
